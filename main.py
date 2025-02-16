3c10d0e25838f6ed93d194824104c91f5e1fa73a
Elideus <aaron@elideus.net>
2025-02-16 11:51:53 -0800

    ffmpeg version checking api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from contextlib import asynccontextmanager
-import aiohttp, json
+import aiohttp, asyncio
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +13,23 @@ router = APIRouter()
 @router.get("/message")
 async def get_message(request: Request):
   return {"message": request.app.state.message}
+
+@router.get("/ffmpeg")
+async def get_ffmpeg():
+  try:
+    process = await asyncio.create_subprocess_exec(
+      "ffmpeg", "-version",
+      stdout=asyncio.subprocess.PIPE,
+      stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    if stdout:
+      version_line = stdout.decode().splitlines()[0]
+    else:
+      version_line = stderr.decode().splitlines()[0]
+    return {"ffmpeg_version": version_line}
+  except Exception as e:
+    raise HTTPException(status_code=500, detail="Error checking ffmpeg: {e}")
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router, prefix="/api")
