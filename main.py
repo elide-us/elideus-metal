889d16b98430ab21c877530b9aa2f27951f8f287
Elideus <aaron@elideus.net>
2025-02-15 18:52:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,32 @@ async def fetch_message():
 @app.get("/")
 async def get_root():
   message = await fetch_message()
-  return message
+  html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Metal Emoji</title>
+      <style>
+        html, body {
+          bheight: 100%;
+          margin: 0;
+        }
+        body {
+          background-color: #333;
+          color: black;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .message {
+          font-size: 20vh;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="message">{{message}}</div>
+    </body>
+    </html>
+  """
+  return html
